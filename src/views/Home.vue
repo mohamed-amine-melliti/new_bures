51c0b5b9e65b5c07308dacaff0158b9d34ac91d6
Administrator <gitlab_admin_24ef4b@example.com>
2025-04-05 20:46:17 +0100

    05/04

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,11 +6,11 @@
 </template>
 
 <script lang="ts">
-import MapView from '@/components/mapview/MapView.vue';
-import SearchBar from '@/components/searchbar/SearchBar.vue';
-import MyLocationBtn from '@/components/my-location-btn/MyLocationBtn.vue';
-import TripDetails from '@/components/trip-details/TripDetails.vue';
-import { usePlacesStore } from '@/composables';
+import MapView from '../components/mapview/MapView.vue';
+import SearchBar from '../components/searchbar/SearchBar.vue';
+import MyLocationBtn from '../components/my-location-btn/MyLocationBtn.vue';
+import TripDetails from '../components/trip-details/TripDetails.vue';
+import { usePlacesStore } from '../composables';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
